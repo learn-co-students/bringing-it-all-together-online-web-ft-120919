4a44a79bc1c3690faec99f19301515552a9819ba
github username <github email address>
2020-01-22 13:06:06 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,15 +70,9 @@ class Dog
 
     def self.find_or_create_by(name:, breed:)
         dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-
-        # sql = <<-SQL
-        #     SELECT *
-        #     FROM dogs
-        #     WHERE name = ? AND breed = ?
-        # SQL
         if !dog.empty?
             dog_info = dog[0]
-            dog = self.new(dog_info[0], dog_info[1], dog_info[2])
+            dog = self.new(id: dog_info[0], name: dog_info[1], breed: dog_info[2])
           else
             dog = self.create(name: name, breed: breed)
           end
